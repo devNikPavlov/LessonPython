70c3969b023d1ce0c83e38145caa32fd5311892a
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-04 10:55:49 +0300

    Books02. exp01. les02

---
+++ Books02/exp01/les02/01_hello_world.py
@@ -0,0 +1,10 @@
+# Переменные
+
+message = 'Hello World!'
+print(message)
+
+message = 'Hello Python'
+print(message)
+
+# Вы можете в любой момент изменить значение переменной в совей программе; Python постоянно отслеживает его текущее
+# состояние
