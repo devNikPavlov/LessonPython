65e044fb5e26ec3492e2639a5983f1b693f65138
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-04 13:04:06 +0300

    Books02. exp01. les03

---
+++ Books02/exp01/les03/bicycles.py
@@ -0,0 +1,30 @@
+# Списки
+
+bicycles = ['terk', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# Обращение к элементам списка
+print(bicycles[0])
+# можно и так
+print(bicycles[0].title())
+
+# обращение к последнему элементу списка
+print(bicycles[-1])
+print(bicycles[-2])
+
+# Использование отдельных элементов из списка
+message = f'My first bicycle was a {bicycles[0].title()}.'
+print(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
