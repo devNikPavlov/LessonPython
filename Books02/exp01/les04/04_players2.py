b001e8dd7560e6b0ba29de31eabaab498dcbcec9
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-10 16:17:37 +0300

    Books02. exp01. les04_4

---
+++ Books02/exp01/les04/04_players2.py
@@ -0,0 +1,14 @@
+# перебор содержимого сегмента
+
+players = [
+    'charles',
+    'martina',
+    'michael',
+    'florence',
+    'ali'
+]
+
+print('Here are the first three players on my team: ')
+for player in players[:3]:
+    print(player.title())
+
