84a59bd0b84caacc8345a15afebb815f7017225a
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-05-14 11:18:49 +0300

    02_pythonclub. - Les_01

---
+++ 02_pythonclub/les_01/main.py
@@ -0,0 +1,34 @@
+a = 2
+b = 3
+
+c = a + b
+d = a - b
+print(c)
+print(d)
+
+z = a * b
+x = a / b
+print(z)
+print(x)
+
+q = a ** b
+w = a % b
+print(q)
+print(w)
+
+e = 'a'
+r = 'b'
+t = e + r
+print(t)
+y = e * 3
+print(y)
+
+i = 'строка'
+o = i[0]
+print(o)
+
+p = i[1:3]
+print(p)
+
+l = len(i)
+print(l)
