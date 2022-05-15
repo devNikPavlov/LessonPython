69e968c7a6e96ee9fd4172786be02bca0b095687
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-05-15 14:39:37 +0300

    02_pythonclub. - Les_04

---
+++ 02_pythonclub/les_04/if_main.py
@@ -0,0 +1,9 @@
+# проверка условий
+
+timsAnswer = 1
+correctAnswer = 1
+score = 0
+if timsAnswer == correctAnswer:
+    print('Ты ответил правельно!')
+    score = score + 1
+print('Спасибо за игру.')
