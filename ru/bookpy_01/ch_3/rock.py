f2a0df32bd0f6f08f5525a90dfd3891bb909639e
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-08-11 19:16:28 +0300

    ru.bookpy_01. Создание 2 проекта, дополнение

--- ru/bookpy_01/ch_3/rock.py
+++ ru/bookpy_01/ch_3/rock.py
@@ -8,5 +8,12 @@ import random
 # random.randint(0, 2)
 random_choice = random.randint(0, 2)
 
-print('Компьютер выбирает', random_choice)
+if random_choice == 0:
+    computer_choice = 'камень'
+elif random_choice == 1:
+    computer_choice = 'бумага'
+else:
+    computer_choice = 'ножницы'
+
+print('Компьютер выбирает', computer_choice)
 
